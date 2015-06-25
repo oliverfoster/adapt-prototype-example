59b95fad0e01c9051f4f0caffed2b80065e6037b
Oliver Foster <oliver.foster@kineo.com>
2015-06-25 15:05:59 +0100

    Update

--- prototype/schemas/schemas.js
+++ prototype/schemas/schemas.js
@@ -1,9 +1,14 @@
 var schemas = {
 	"blank": true,
 	"text": true,
+	"graphic": true,
 	"accordion": true,
 	"narrative": true,
 	"hotgraphic": true,
 	"mcq": true,
-	"gmcq": true
+	"gmcq": true,
+	"matching": true,
+	"textinput": true,
+	"slider": true,
+	"media": true
 };
